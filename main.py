0da12d40913b2a8598a8817cf65ef68a1e61514a
jai11k <jai11simran@gmail.com>
2023-02-28 00:52:33 +0530

    made a new venv and facing minor erorr :(

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI,Response,status,HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -17,8 +17,14 @@ my_posts=[{"title":"titleValue1","content":"contentValue1","id":1 },
 
 def find_post(id):
     for post in my_posts:
-        if post["id"]==id:
+        if post['id']==id:
             return post
+        
+def find_index_post(id):
+    for i,post in enumerate(my_posts):
+        if post['id']==id:
+            return i
+        
 
 @app.get("/")
 def read_root():
@@ -36,16 +42,30 @@ def read_root():
 def get_posts():
     return {"data": my_posts}
 
-#Get
-@app.post("/posts")
+#Create
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_user(post: Post):
     post_dict=post.dict()
     post_dict['id']=randrange(0,1000000)
     my_posts.append(post_dict)
     return {"response": post_dict}
 
-#post 
+#Get single post
 @app.post("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {id} was not found")
+        # response.status_code=status.HTTP_404_NOT_FOUND
+        # return {"message":f"post with id {id} was not found"}
     return {"data": post}
+
+
+#Delete single post
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+
+    index=find_index_post(id)
+    my_posts.pop(index)
+    
+    return {"message": f"post was successfully deleted"}
